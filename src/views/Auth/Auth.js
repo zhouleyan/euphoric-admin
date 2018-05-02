0217121e7b244a22bdec8fc7378bd061209828af
周乐延 <lyzhou@fiberhome.com>
2018-05-02 20:38:27 +0800

    start auth.login

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,14 +1,39 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import DocumentTitle from 'react-document-title';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { getRoutes } from '../../utils';
 
 class Auth extends React.PureComponent {
+  getPageTitle = () => {
+    const {
+      routerData,
+      location: { pathname }
+    } = this.props;
+    let title = 'EuphoricAdmin';
+    if (routerData[pathname] && routerData[pathname].name) {
+      title = `${routerData[pathname].name} - EuphoricAdmin`;
+    }
+    return title;
+  };
   render() {
-    return <div>Auth</div>;
+    const { routerData, match } = this.props;
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <Switch>
+          {getRoutes(match.path, routerData).map(item => (
+            <Route
+              key={item.key}
+              path={item.path}
+              component={item.component}
+              exact={item.exact}
+            />
+          ))}
+          <Redirect exact from="/auth" to="/auth/login" />
+        </Switch>
+      </DocumentTitle>
+    );
   }
 }
-
-// Auth.propTypes = {
-
-// };
 
 export default Auth;
---
+++ src/views/Auth/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './auth.less';
+
+class Login extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        Login
+      </div>
+    );
+  }
+}
+
+export default Login;
